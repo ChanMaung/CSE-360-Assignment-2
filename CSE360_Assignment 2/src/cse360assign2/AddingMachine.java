8e9cc1be69190e8ae1829d0dec18c0feee09c92c
Chan Maung <cmaung@asu.edu>
2019-10-08 18:50:01 -0700

    Second commit with changes.

--- CSE360_Assignment 2/src/cse360assign2/AddingMachine.java
+++ CSE360_Assignment 2/src/cse360assign2/AddingMachine.java
@@ -3,28 +3,67 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
+	private String history = "0";
 	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
 	}
 	
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
+	/**
+	 *  Add the parameter to the variable total.
+	 *  
+	 *  @param	value	the value to add.
+	 */
 	public void add (int value) {
 		
+		total = value + total;
+		history = history + " + " + value; 
+		
 	}
 	
+	/**
+	 *  Subtract the parameter from the variable total.
+	 *  
+	 *  @param	value	the value to subtract.
+	 */
 	public void subtract (int value) {
 		
-	}
+		total = total - value;
+		history = history + " - " + value; 
 		
+	}
+	
+	/**
+	 *  Return a list of operations in order stored in String history.
+	 */	
 	public String toString () {
-		return "";
+		System.out.println(history);
+		return history;
 	}
 
 	public void clear() {
+		
+		total = 0;
+		history = "0";
 	
 	}
+	
+	
+
+
+public static void main (String[] args) {
+	
+	AddingMachine test = new AddingMachine();
+	test.add(4);
+	test.subtract(2);
+	test.add(5);
+	test.toString();
+	}
 }
+
+
+
